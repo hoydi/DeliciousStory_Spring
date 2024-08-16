84fd33067aca70fc04a809a459261e70ff4abf4d
hoydi <hoydi110@gmail.com>
2024-08-16 16:36:14 +0900

    layout

---
+++ src/main/java/com/i5/ds/MainController.java
@@ -0,0 +1,12 @@
+package com.i5.ds;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
---
+++ src/main/java/com/i5/ds/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.i5.ds.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests(authorize -> authorize
+//                        .requestMatchers("/").permitAll() // "/" 경로에 대해 인증 없이 접근 가능하도록 설정
+//                        .anyRequest().authenticated() // 나머지 요청에 대해서는 인증 필요
+//                )
+//                .formLogin(Customizer.withDefaults()) // 기본 폼 로그인을 사용하려면 이 설정을 유지
+//                .logout(Customizer.withDefaults()); // 기본 로그아웃을 사용하려면 이 설정을 유지
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll() // 모든 요청에 대해 인증 없이 접근 가능하도록 설정
+                )
+                .formLogin(Customizer.withDefaults())
+                .logout(Customizer.withDefaults());
+        return http.build();
+    }
+}
