c387fea56a12af93f87f61fb96495a451329df2d
hoydi <hoydi110@gmail.com>
2024-08-26 12:12:09 +0900

    long->string으로수정

--- src/main/java/com/i5/ds/User/UserRepository.java
+++ src/main/java/com/i5/ds/User/UserRepository.java
@@ -4,7 +4,7 @@ package com.i5.ds.User;
 import com.i5.ds.User.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, String> {
     User findByUserId(String userId);
 
     User findByUserEmail(String userEmail);
