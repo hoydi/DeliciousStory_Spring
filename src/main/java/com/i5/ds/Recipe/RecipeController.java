b2e741970dad1a15042151bb21434b760cd78a8d
hoydi <hoydi110@gmail.com>
2024-08-29 10:16:08 +0900

    공식레시피 레이지로딩 구현

--- src/main/java/com/i5/ds/Recipe/RecipeController.java
+++ src/main/java/com/i5/ds/Recipe/RecipeController.java
@@ -3,6 +3,7 @@ package com.i5.ds.Recipe;
 import com.i5.ds.Recipe.Recipe;
 import com.i5.ds.Recipe.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -27,10 +28,15 @@ public class RecipeController {
     }
 
     @GetMapping("/site_recipe")
-    public String getAllRecipes(Model model) {
-        List<Recipe> recipes = recipeService.getAllRecipes();
-        model.addAttribute("recipes", recipes.stream().limit(10).collect(Collectors.toList()));
-
+    public String getRecipes(Model model,
+                             @RequestParam(defaultValue = "0") int page,
+                             @RequestParam(defaultValue = "20") int size) {
+        Page<Recipe> recipePage = recipeService.getRecipes(page, size);
+        model.addAttribute("recipes", recipePage.getContent());
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", recipePage.getTotalPages());
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println(recipePage.getTotalPages());
         return "pages/siteRecipe/siteRecipe_list";
     }
 
--- src/main/java/com/i5/ds/Recipe/RecipeService.java
+++ src/main/java/com/i5/ds/Recipe/RecipeService.java
@@ -3,6 +3,9 @@ package com.i5.ds.Recipe;
 import com.i5.ds.Recipe.Recipe;
 import com.i5.ds.Recipe.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,8 +13,16 @@ import java.util.List;
 @Service
 public class RecipeService {
 
-    @Autowired
-    private RecipeRepository recipeRepository;
+    private final RecipeRepository recipeRepository;
+
+    public RecipeService(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    public Page<Recipe> getRecipes(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return recipeRepository.findAll(pageable);
+    }
 
     public List<Recipe> getAllRecipes() {
         return recipeRepository.findAll();
