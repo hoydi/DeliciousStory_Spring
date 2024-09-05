1885bbb1a0766a71c56a778f22f7b5d2fe52e3b8
hoydi <hoydi110@gmail.com>
2024-09-05 09:46:35 +0900

    메인페이지 공식레시피추가

--- src/main/java/com/i5/ds/MainController.java
+++ src/main/java/com/i5/ds/MainController.java
@@ -1,29 +1,34 @@
 package com.i5.ds;
 
+import com.i5.ds.Recipe.SiteRecipe.Recipe;
+import com.i5.ds.Recipe.SiteRecipe.RecipeService;
+import com.i5.ds.Recipe.UserRecipe.UserRecipe;
+import com.i5.ds.Recipe.UserRecipe.UserRecipeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+
+    @Autowired
+    private RecipeService recipeService;
+    @Autowired
+    private UserRecipeService userRecipeService;
+
+
     @GetMapping("/")
-    public String index() {
-        return "index";
+    public String index(Model model) {
+        // 첫 번째 페이지에서 4개의 레시피를 가져옴 ( 추가해도됨 )
+        Page<Recipe> recipePage = recipeService.getRecipes(0, 4);
+        // 첫 번째 페이지에서 4개의 유저 레시피를 가져옴
+        Page<UserRecipe> userRecipePage = userRecipeService.getUserRecipes(0, 4);
+
+        // 모델에 레시피와 유저 레시피 리스트를 추가
+        model.addAttribute("recipes", recipePage.getContent());
+        model.addAttribute("userRecipes", userRecipePage.getContent());
+        return "index"; // index.html 페이지로 리턴
     }
-
-    //--------레시피류 임시 매핑--------------------------------
-
-//    @GetMapping("/site_recipe_page")
-//    public String siteRecipeDetail() {
-//        return "pages/siteRecipe/siteRecipe_detail"; // 뷰 파일 이름
-//    }
-//
-//    @GetMapping("/user_recipe")
-//    public String userRecipeList() {
-//        return "pages/userRecipe/userRecipe_list"; // 뷰 파일 이름
-//    }
-//
-//    @GetMapping("/user_recipe_page")
-//    public String userRecipeDetail() {
-//        return "pages/userRecipe/userRecipe_detail"; // 뷰 파일 이름
-//    }
 }
