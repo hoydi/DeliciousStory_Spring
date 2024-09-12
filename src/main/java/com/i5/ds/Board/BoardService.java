e1532cfde65ce3ad8ede71ecd59208d3e9d7f853
kim gayoung <kky36907960@gmail.com>
2024-09-12 09:14:49 +0900

    보드 에러 임시수정

--- src/main/java/com/i5/ds/Board/BoardService.java
+++ src/main/java/com/i5/ds/Board/BoardService.java
@@ -12,22 +12,6 @@ public class BoardService {
 	@Autowired
 	private BoardRepository boardRepository;
 
-	public List<Board> getAllBoards() {
-		return boardRepository.findAll();
-	}
-
-	public Optional<Board> getBoardById(Integer id) {
-		return boardRepository.findById(id);
-	}
-
-	public Board saveBoard(Board board) {
-		return boardRepository.save(board);
-	}
-
-	public void deleteBoard(Integer id) {
-		boardRepository.deleteById(id);
-	}
-
 	public List<Board> getBoardsByUserId(String userId) {
 		return boardRepository.findByUserId(userId);
 	}
